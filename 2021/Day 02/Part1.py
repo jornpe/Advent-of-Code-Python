2dffdd0501c252a2b6a9a437907cc7de2f5254f3
Jørn Aleksander Pettersen <jorn.aleksander.pettersen@kongsbergdigital.com>
2022-11-27 09:49:44 +0100

    Refactoring of day 1 and 2

--- 2021/Day 02/Part1.py
+++ 2021/Day 02/Part1.py
@@ -1,13 +1,13 @@
 with open("input.txt") as f:
-    input = [ l.rstrip().split(' ') for l in f]
+    input = [(cmd, int(arg)) for cmd, arg in [line.split(' ') for line in f]]
 
 forward = 0
 up = 0
 down = 0
 
 for move in input:
-    if move[0].startswith('forward'):   forward += int(move[1])
-    if move[0].startswith('up'):        up += int(move[1])
-    if move[0].startswith('down'):      down += int(move[1])
+    if move[0].startswith('forward'):   forward += move[1]
+    if move[0].startswith('up'):        up += move[1]
+    if move[0].startswith('down'):      down += move[1]
 
 print(forward * (down - up))
--- 2021/Day 02/Part2.py
+++ 2021/Day 02/Part2.py
@@ -1,5 +1,6 @@
 with open("input.txt") as f:
-    input = [ l.rstrip().split(' ') for l in f]
+    input = [(cmd, int(arg)) for cmd, arg in [line.split(' ') for line in f]]
+
 
 forward = 0
 depth = 0
@@ -7,9 +8,9 @@ aim = 0
 
 for move in input:
     if move[0].startswith('forward'):   
-        forward += int(move[1])
-        depth += aim * int(move[1])
-    if move[0].startswith('up'): aim -= int(move[1])
-    if move[0].startswith('down'): aim += int(move[1])
+        forward += move[1]
+        depth += aim * move[1]
+    if move[0].startswith('up'): aim -= move[1]
+    if move[0].startswith('down'): aim += move[1]
 
 print(depth * forward)
