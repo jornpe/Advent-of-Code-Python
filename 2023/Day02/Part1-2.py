2cd8405156dbb01767cff242fbb35c1c1a2fba09
AzureAD\JornPettersen <jorn.aleksander.pettersen@kongsbergdigital.com>
2023-12-02 07:56:39 +0100

    ⭐⭐ Day 2 2023

---
+++ 2023/Day02/Part1-2.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import re
+
+with open(Path(__file__).with_name('input.txt')) as f:
+    games = [line for line in f.read().split('\n')]
+
+part1 = 0
+part2 = 0
+
+for game_id, game in enumerate(games, 1):
+    draws = game.split(':')[1]
+    colors = re.findall('(\d+) (blue|red|green)', draws)
+
+    red = max([int(r[0]) for r in colors if r[1] == 'red'])
+    blue = max([int(r[0]) for r in colors if r[1] == 'blue'])
+    green = max([int(r[0]) for r in colors if r[1] == 'green'])
+
+    if red <= 12 and green <= 13 and blue <= 14:
+        part1 += game_id
+
+    part2 += red * blue * green
+
+
+print(f'⭐ Part 1: {part1}')
+print(f'⭐⭐ Part 2: {part2}')
--- DayXX/Part1.py
+++ DayXX/Part1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 with open(Path(__file__).with_name('test.txt')) as f:
-    input = [line for line in f.read().split('\n')]
+    lines = [line for line in f.read().split('\n')]
 
 answer = 0
 print(f'⭐ Part 1: {answer}')
