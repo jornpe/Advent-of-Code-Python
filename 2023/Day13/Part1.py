e8c34050f50f12922dee984d9d876e3f3fa39b6e
AzureAD\JornPettersen <jorn.aleksander.pettersen@kongsbergdigital.com>
2023-12-13 14:26:55 +0100

    ⭐⭐ Day 13

---
+++ 2023/Day13/Part1.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from itertools import pairwise
+
+with open(Path(__file__).with_name('input.txt')) as f:
+    patterns = [line.split() for line in f.read().split('\n\n')]
+
+
+def get_pattern_value(pattern: list, multiplier: int) -> int:
+    for row, lines in enumerate(pairwise(pattern)):
+        if lines[0] == lines[1]:
+            if all([pattern[l1] == pattern[l2] for l1, l2 in zip(range(row - 1, -1, -1), range(row + 2, len(pattern)))]):
+                return (row + 1) * multiplier
+    return 0
+
+
+answer = 0
+for pattern in patterns:
+    vertical = get_pattern_value(list(zip(*pattern[::-1])), 1)
+    horizontal = get_pattern_value(pattern, 100)
+    answer += vertical + horizontal
+
+
+print(f'⭐ Part 1: {answer}')
---
+++ 2023/Day13/Part2.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from itertools import pairwise
+
+with open(Path(__file__).with_name('input.txt')) as f:
+    patterns = [line.split() for line in f.read().split('\n\n')]
+
+
+def get_pattern_value(pattern: list, multiplier: int) -> int:
+    for row, lines in enumerate(pairwise(pattern)):
+        if 1 == sum(sum(1 for a, b in zip(pattern[l1], pattern[l2]) if a != b) for l1, l2 in zip(range(row, -1, -1), range(row + 1, len(pattern)))):
+            return (row + 1) * multiplier
+    return 0
+
+
+answer = 0
+for pattern in patterns:
+    vertical = get_pattern_value(list(zip(*pattern[::-1])), 1)
+    horizontal = get_pattern_value(pattern, 100)
+    answer += vertical + horizontal
+
+
+print(f'⭐⭐ Part 2: {answer}')
