996b4952a592a5225dba6e5445aadb01c6093f82
AzureAD\JornPettersen <jorn.aleksander.pettersen@kongsbergdigital.com>
2023-12-26 09:51:28 +0100

    ⭐⭐ Day 24 part 2

--- 2023/Day24/Part2.py
+++ 2023/Day24/Part2.py
@@ -2,37 +2,30 @@ from pathlib import Path
 import time
 import re
 import sympy
+from sympy import Eq
 
 start_time = time.time()
 with open(Path(__file__).with_name('input.txt')) as f:
-    lines = [line for line in f.read().split('\n')]
+    hails = [line for line in f.read().split('\n')]
 
 xr, yr, zr, vxr, vyr, vzr = sympy.symbols("xr, yr, zr, vxr, vyr, vzr")
 
 equations = []
 
-# Her we build up all the equations needed. This is probably not necessarry to do for all the hails as after a few
-# there will most likly only be one solution.
+# Her we build up all the equations needed. This only needs to be doe for the 3 first.
 # The equations start's with this (where r = rock and h = hail):
 #
 # xh + t*vxh = xr + t*vxr
 # yh + t*vyh = yr + t*vyr
 # zh + t*vzh = zr + t*vzr
 #
-# which means: t = (xr - xh) / (vxh - vxr) = (yr - yh) / (vyh - vyr) = (zr - zh) / (vzh - vzr)
-#
-# Since symby needs equations = 0, then we need to split into 2 equations:
-#
-# (xr - xh) / (vxh - vxr) - (yr - yh) / (vyh - vyr) = 0
-# (xr - xh) / (vxh - vxr) - (zr - zh) / (vzh - vzr) = 0
-#
-# We do not need this: (yr - yh) / (vyh - vyr) - (zr - zh) / (vzh - vzr) = 0
-# since it's given by the 2 other equations.
 
-for l1 in lines:
-    xh, yh, zh, vxh, vyh, vzh = list(map(int, re.findall(r'(-?\d+)', l1)))
-    equations.append((xr - xh) * (vyh - vyr) - (yr - yh) * (vxh - vxr))
-    equations.append((xr - xh) * (vzh - vzr) - (zr - zh) * (vxh - vxr))
+for idx, hail in enumerate(hails[:3]):
+    t = sympy.symbols(f't{idx}')
+    xh, yh, zh, vxh, vyh, vzh = list(map(int, re.findall(r'(-?\d+)', hail)))
+    equations.append(Eq(xh + t * vxh, xr + t * vxr))
+    equations.append(Eq(yh + t * vyh, yr + t * vyr))
+    equations.append(Eq(zh + t * vzh, zr + t * vzr))
 
 solution = sympy.solve(equations)[0]
 answer = solution[xr] + solution[yr] + solution[zr]
