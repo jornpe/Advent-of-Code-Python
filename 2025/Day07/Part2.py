4bdc8f04a7116c5b9ba5564541813bef24b21560
Jørn Pettersen <joern.pettersen@gmail.com>
2025-12-07 07:55:48 +0100

    ⭐⭐ More fun

--- 2025/Day07/Part2.py
+++ 2025/Day07/Part2.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 import time
 
@@ -5,26 +6,18 @@ start_time = time.time()
 with open(Path(__file__).with_name('input.txt')) as f:
     input = f.read().split('\n')
 
-timelines = {(0, col): 1 for col, c in enumerate(input[0]) if c == 'S'}
+timelines = {col: 1 for col, c in enumerate(input[0]) if c == 'S'}
 splitters = set((row, col) for row, line in enumerate(input) for col, c in enumerate(line) if c == '^')
 
-
-def update_timelines(pos: tuple, n: int, timelines: dict):
-    if pos in timelines:
-        timelines[pos] += n
-    else:
-        timelines[pos] = n
-
-
-for _ in range(len(input)):
-    tl = timelines
-    timelines = {}
-    for (row, col), n  in tl.items():
+for row in range(len(input)):
+    next_timelines = defaultdict(int)
+    for col, n  in timelines.items():
         if (row+1, col) in splitters:
-            update_timelines((row+1, col-1), n, timelines)
-            update_timelines((row+1, col+1), n, timelines)
+            next_timelines[col-1] += n
+            next_timelines[col+1] += n
         else:
-            update_timelines((row + 1, col), n, timelines)
+            next_timelines[col] += n
+    timelines = next_timelines
 
 answer = sum(n for n in timelines.values())
 print(f'⭐⭐ Part 2: {answer} ; run time: {int((time.time() - start_time) * 1000)}ms')
